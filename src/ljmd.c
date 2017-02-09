8a424a90a4cfd04160d1b43fe7d86c1658fb66dc
GivAlz <giovannialzetta@hotmail.it>
2017-02-09 13:12:04 +0100

    Splitted out prototypes

--- src/ljmd.c
+++ src/ljmd.c
@@ -11,24 +11,15 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include "prototypes.h"
+
 /* generic file- or pathname buffer length */
 #define BLEN 200
 
 /* a few physical constants */
-const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
-const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
+extern const double kboltz;     /* boltzman constant in kcal/mol/K */
+extern const double mvsq2e; /* m*v^2 in kcal/mol */
 
-/* structure to hold the complete information 
- * about the MD system */
-struct _mdsys {
-    int natoms,nfi,nsteps;
-    double dt, mass, epsilon, sigma, box, rcut;
-    double ekin, epot, temp;
-    double *rx, *ry, *rz;
-    double *vx, *vy, *vz;
-    double *fx, *fy, *fz;
-};
-typedef struct _mdsys mdsys_t;
 
 /* helper function: read a line and then return
    the first string with whitespace stripped off */
---
+++ src/prototypes.h
@@ -0,0 +1,23 @@
+#ifndef PROT_H
+#define PROT_H
+
+/* generic file- or pathname buffer length */
+#define BLEN 200
+
+/* a few physical constants */
+const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
+const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
+
+/* structure to hold the complete information 
+ * about the MD system */
+struct _mdsys {
+    int natoms,nfi,nsteps;
+    double dt, mass, epsilon, sigma, box, rcut;
+    double ekin, epot, temp;
+    double *rx, *ry, *rz;
+    double *vx, *vy, *vz;
+    double *fx, *fy, *fz;
+};
+typedef struct _mdsys mdsys_t;
+
+#endif
