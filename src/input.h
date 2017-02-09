0772c0cc29e809617d14984f21b82074fdd8a6d3
GivAlz <giovannialzetta@hotmail.it>
2017-02-09 13:25:01 +0100

    Added input

---
+++ src/input.h
@@ -0,0 +1,11 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int get_a_line(FILE *fp, char *buf);
+
+#endif
--- src/ljmd.c
+++ src/ljmd.c
@@ -12,43 +12,15 @@
 #include <math.h>
 
 #include "prototypes.h"
+#include "input.h"
 
-/* generic file- or pathname buffer length */
-#define BLEN 200
 
 /* a few physical constants */
 extern const double kboltz;     /* boltzman constant in kcal/mol/K */
 extern const double mvsq2e; /* m*v^2 in kcal/mol */
 
 
-/* helper function: read a line and then return
-   the first string with whitespace stripped off */
-static int get_a_line(FILE *fp, char *buf)
-{
-    char tmp[BLEN], *ptr;
 
-    /* read a line and cut of comments and blanks */
-    if (fgets(tmp,BLEN,fp)) {
-        int i;
-
-        ptr=strchr(tmp,'#');
-        if (ptr) *ptr= '\0';
-        i=strlen(tmp); --i;
-        while(isspace(tmp[i])) {
-            tmp[i]='\0';
-            --i;
-        }
-        ptr=tmp;
-        while(isspace(*ptr)) {++ptr;}
-        i=strlen(ptr);
-        strcpy(buf,tmp);
-        return 0;
-    } else {
-        perror("problem reading input");
-        return -1;
-    }
-    return 0;
-}
  
 /* helper function: zero out an array */
 static void azzero(double *d, const int n)
