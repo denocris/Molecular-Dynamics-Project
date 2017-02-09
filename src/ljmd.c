234d989123ea9a16a0f30f41331d551e598cb922
GivAlz <giovannialzetta@hotmail.it>
2017-02-09 13:39:34 +0100

    Splitted verlet

--- src/ljmd.c
+++ src/ljmd.c
@@ -16,42 +16,12 @@
 #include "utilities.h"
 #include "force.h"
 #include "output.h"
-
+#include "verlet1.h"
 
 
 /* a few physical constants */
 const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
 const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
-
-
- 
-/* velocity verlet */
-static void velverlet(mdsys_t *sys)
-{
-    int i;
-
-    /* first part: propagate velocities by half and positions by full step */
-    for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
-        sys->rx[i] += sys->dt*sys->vx[i];
-        sys->ry[i] += sys->dt*sys->vy[i];
-        sys->rz[i] += sys->dt*sys->vz[i];
-    }
-
-    /* compute forces and potential energy */
-    force(sys);
-
-    /* second part: propagate velocities by another half step */
-    for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
-    }
-}
-
-
 
 
 /* main */
---
+++ src/verlet2.h
@@ -0,0 +1,8 @@
+#ifndef VERLET2_H
+#define VERLET2_H
+
+#include "prototypes.h"
+
+void verlet2(mdsys_t *sys);
+
+#endif
