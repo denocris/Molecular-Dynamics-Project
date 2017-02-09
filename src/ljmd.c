f57df3102408b7b150912ba94a55a41ee3b59d30
GivAlz <giovannialzetta@hotmail.it>
2017-02-09 13:32:12 +0100

    Added force

--- src/ljmd.c
+++ src/ljmd.c
@@ -16,75 +16,18 @@
 #include "utilities.h"
 
 
-/* a few physical constants */
-extern const double kboltz;     /* boltzman constant in kcal/mol/K */
-extern const double mvsq2e; /* m*v^2 in kcal/mol */
 
+
+
+/* a few physical constants */
+const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
+const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
 
 
  
 
-/* helper function: apply minimum image convention */
-static double pbc(double x, const double boxby2)
-{
-    while (x >  boxby2) x -= 2.0*boxby2;
-    while (x < -boxby2) x += 2.0*boxby2;
-    return x;
-}
 
 
-/* compute kinetic energy */
-static void ekin(mdsys_t *sys)
-{   
-    int i;
-
-    sys->ekin=0.0;
-    for (i=0; i<sys->natoms; ++i) {
-        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
-    }
-    sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
-}
-
-/* compute forces */
-static void force(mdsys_t *sys) 
-{
-    double r,ffac;
-    double rx,ry,rz;
-    int i,j;
-
-    /* zero energy and forces */
-    sys->epot=0.0;
-    azzero(sys->fx,sys->natoms);
-    azzero(sys->fy,sys->natoms);
-    azzero(sys->fz,sys->natoms);
-
-    for(i=0; i < (sys->natoms); ++i) {
-        for(j=0; j < (sys->natoms); ++j) {
-
-            /* particles have no interactions with themselves */
-            if (i==j) continue;
-            
-            /* get distance between particle i and j */
-            rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
-            ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
-            rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-            r = sqrt(rx*rx + ry*ry + rz*rz);
-      
-            /* compute force and energy if within cutoff */
-            if (r < sys->rcut) {
-                ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
-                                         +6*pow(sys->sigma/r,6.0)/r);
-                
-                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
-                                               -pow(sys->sigma/r,6.0));
-
-                sys->fx[i] += rx/r*ffac;
-                sys->fy[i] += ry/r*ffac;
-                sys->fz[i] += rz/r*ffac;
-            }
-        }
-    }
-}
 
 /* velocity verlet */
 static void velverlet(mdsys_t *sys)
--- src/prototypes.h
+++ src/prototypes.h
@@ -3,11 +3,9 @@
 
 /* generic file- or pathname buffer length */
 #define BLEN 200
-
 /* a few physical constants */
-const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
-const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
-
+extern const double kboltz;     /* boltzman constant in kcal/mol/K */
+extern const double mvsq2e; /* m*v^2 in kcal/mol */
 /* structure to hold the complete information 
  * about the MD system */
 struct _mdsys {
